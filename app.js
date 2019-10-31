3065aaf788d954b393b1c08663475d4c41d342df
David McGarvey <davidjmcgarvey+github@gmail.com>
2019-10-31 09:40:09 -0700

    link json file

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ app.set('view engine', 'pug');
 const routes = require('./routes');
 app.use(routes);
 
-
-
 // Setup local host
 app.listen(3000, () => {
   console.log(`The app is running on localhost:3000, dawg!`);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { projects } = require('../data/projectData.json');
+ 
 
+
+router.get('/', (req, res, next) => {
+  const name = projects[0].project_name;
+  const templateData = { name };
+  res.render('about', templateData);
+});
 
 
 module.exports = router;
