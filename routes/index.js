a84700b7833d206ec06f99d1a06d848e67b0284c
David McGarvey <davidjmcgarvey+github@gmail.com>
2019-11-01 10:16:28 -0700

    refactor img dirs

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const { projects } = require('../data/projectData.json');
- 
 
+// router.get('/', (req, res, next) => {
+//   const name = projects[0].project_name;
+//   const templateData = { name };
+//   res.render('about', templateData);
+// });
+
+// router.get('/about', (req, res, next) => {
+//   const name = projects[0].project_name;
+//   const templateData = { name };
+//   res.render('about', templateData);
+// });
 
 router.get('/', (req, res, next) => {
-  const name = projects[0].project_name;
-  const templateData = { name };
-  res.render('about', templateData);
+  res.render('index');
+  // res.redirect('/about/');
 });
 
+router.get('/about', (req, res, next) => {
+  res.render('about');
+});
+
+router.get('/project/:id', (req, res, next) => {
+  const { id } = req.params;
+  const name = projects[id].project_name;
+
+  const templateData = { id, name };
+  res.render('project', templateData);
+});
 
 module.exports = router;
